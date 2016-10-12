5cec88a31e55cdc2872d0d6764e2629b873959d2
primaeval <primaeval@users.noreply.github.com>
2016-10-12 18:32:21 +0200

    ls lists image location

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def ls_list(url,type,export):
             continue
 
         img_url = ''
-        img_match = re.search(r'"(http://ia.media-imdb.com/images.*?)"', list_item, flags=(re.DOTALL | re.MULTILINE))
+        img_match = re.search(r'"(https://images-na.ssl-images-amazon.com/images.*?)"', list_item, flags=(re.DOTALL | re.MULTILINE))
         if img_match:
             img = img_match.group(1)
             img_url = re.sub(r'S[XY].*_.jpg','SX344_.jpg',img) #NOTE 344 is Confluence List View width
