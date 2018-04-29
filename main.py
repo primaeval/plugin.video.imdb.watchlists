bc87462ab3b12d60410d97d649dc1cdf0fe597b0
primaeval <primaeval@users.noreply.github.com>
2018-04-29 23:00:04 +0200

    0.0.38

--- main.py
+++ main.py
@@ -388,6 +388,16 @@ def add_to_trakt_collection(type,imdb_id,title):
                 }
             ]
         })
+        if type == "shows":
+            result = Trakt['sync/history'].add({
+                type: [
+                    {
+                        'ids': {
+                            'imdb': imdb_id
+                        }
+                    }
+                ]
+            })
         dialog = xbmcgui.Dialog()
         dialog.notification("Trakt: add to collection",title)
 
